8aec80f1a879e8c072a32b11e04bde9c4097496e
unknown <lyosprogramming@gmail.com>
2020-11-11 18:46:54 +0100

    CORS

--- src/store/actions/index.tsx
+++ src/store/actions/index.tsx
@@ -3,7 +3,12 @@ import axios from "axios"
 import { parse } from "fast-xml-parser"
 import { Book, Author } from "../../objectTemplates/templates"
 var config = {
-  headers: {'Access-Control-Allow-Origin': '*'}
+  headers: [
+    { key: "Access-Control-Allow-Credentials", value: "true" },
+    { key: "Access-Control-Allow-Origin", value: "*" },
+    { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+  ]
 };
 
 export const fetchBooksFromServer = (bookName: string|null|undefined, page: number = 1) => async (
